1c54260127e95cfecea9ed6aff507863391be54c
Matt Kai <42985693+KaiXtr@users.noreply.github.com>
2020-03-31 13:19:38 -0300

    Initial commit pt. I

---
+++ Tiles/Files/laboratory.tsx
@@ -0,0 +1,235 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="laboratory" tilewidth="30" tileheight="30" tilecount="81" columns="9">
+ <image source="../laboratory.png" width="270" height="270"/>
+ <tile id="0">
+  <properties>
+   <property name="TYPE" value="brick"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="TYPE" value="brick"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="TYPE" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="TYPE" value="none"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="TYPE" value="none"/>
+  </properties>
+  <animation>
+   <frame tileid="4" duration="400"/>
+   <frame tileid="9" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="TYPE" value="grass"/>
+  </properties>
+  <animation>
+   <frame tileid="5" duration="400"/>
+   <frame tileid="6" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="TYPE" value="grass"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="TYPE" value="metal"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="TYPE" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="TYPE" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="TYPE" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="TYPE" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="TYPE" value="grass"/>
+  </properties>
+  <animation>
+   <frame tileid="14" duration="400"/>
+   <frame tileid="15" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="TYPE" value="grass"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="TYPE" value="grass"/>
+  </properties>
+  <animation>
+   <frame tileid="18" duration="400"/>
+   <frame tileid="27" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="TYPE" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="TYPE" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="TYPE" value="none"/>
+  </properties>
+  <animation>
+   <frame tileid="21" duration="400"/>
+   <frame tileid="30" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="TYPE" value="none"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="TYPE" value="grass"/>
+  </properties>
+  <animation>
+   <frame tileid="23" duration="400"/>
+   <frame tileid="24" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="TYPE" value="grass"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="TYPE" value="grass"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="TYPE" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="TYPE" value="none"/>
+  </properties>
+  <animation>
+   <frame tileid="29" duration="400"/>
+   <frame tileid="38" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="TYPE" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="TYPE" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="TYPE" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="TYPE" value="grass"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="TYPE" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="TYPE" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="TYPE" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="TYPE" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="TYPE" value="grass"/>
+  </properties>
+  <animation>
+   <frame tileid="45" duration="400"/>
+   <frame tileid="46" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="TYPE" value="grass"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="TYPE" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="TYPE" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="TYPE" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="TYPE" value="grass"/>
+  </properties>
+  <animation>
+   <frame tileid="54" duration="400"/>
+   <frame tileid="63" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="TYPE" value="grass"/>
+  </properties>
+ </tile>
+</tileset>
